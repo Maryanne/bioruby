5c0db14c0cff888645ed27a2a30b50a3bd8648bd
Martin Kuria <martinkuria@gmail.com>
2010-09-18 14:09:42 +0300

    Implemented a method that calls system bepipred binary

--- lib/bio/appl/bepipred.rb
+++ lib/bio/appl/bepipred.rb
@@ -1,46 +1,46 @@
 #
 # = bio/appl/bepipred.rb - Bepipred wrapper
-# 
-# Copyright::   Copyright (C) 2010 
-#               George Githinji <georgkam@gmail.com>
-# License::     The Ruby License
+#
+# Copyright:: Copyright (C) 2010
+# George Githinji <georgkam@gmail.com>
+# License:: The Ruby License
 #
 # $Id:$
 #
 ##############################################################################
-#   BepiPred predicts the location of linear B-cell epitopes in proteins using
-#   a combination of a hidden Markov  model and a propensity scale method. The
-#   method is described in the following article:
+# BepiPred predicts the location of linear B-cell epitopes in proteins using
+# a combination of a hidden Markov model and a propensity scale method. The
+# method is described in the following article:
 #
-#   Improved method for predicting linear B-cell epitopes.
-#   Jens Erik Pontoppidan Larsen, Ole Lund and Morten Nielsen
-#   Immunome Research 2:2, 2006.
-
+# Improved method for predicting linear B-cell epitopes.
+# Jens Erik Pontoppidan Larsen, Ole Lund and Morten Nielsen
+# Immunome Research 2:2, 2006.
+require 'rubygems'
+require 'bio'
 require 'bio/command'
 require 'shellwords'
 
 module Bio
 
   # == Description
-  # 
+  #
   # A wrapper for Bepipred linear B-cell epitope prediction program.
   #
   # === Examples
   #
-  #   require 'bio'
-  #   seq_file = 'test.fasta'
-  #  
-  #   factory = Bio::Bepipred.new(seq_file)
-  #   report = factory.query
-  #   report.class # => Bio::Bepipred::Report
+  # require 'bio'
+  # seq_file = 'test.fasta'
+  #
+  # factory = Bio::Bepipred.new(seq_file)
+  # report = factory.query
+  # report.class # => Bio::Bepipred::Report
   #
 class Bepipred
   autoload :Report, 'bio/appl/bepipred/report'
   
   # Creates a new Bepipred execution wrapper object
-  def initialize(program='bepipred',score_threshold=0.35,file_name)
+  def initialize(program='bepipred',file_name='/usr/local/bepipred-1.0b/test/CHO.fsa')
     @program = program
-    @score_threshold = score_threshold
     @file_name = file_name
   end
   
@@ -52,6 +52,9 @@ class Bepipred
   
   # return the names of the input sequences
   attr_reader :sequence_names
+
+  # expose the internal datastructure so that it can be manipulated directly.
+  attr_accessor :result_list
   
   def sequence_names(file)
     sequence_names = []
@@ -62,32 +65,16 @@ class Bepipred
     end
     sequence_names
   end
-  
-  # TODO create a list of query sequences
-  
-  
+
   #TODO create a commandline as an array cmd
   def make_command
-    cmd = [@program,"-t #{@score_threshold}",@file_name ]
+    cmd = [@program,@file_name ]  
+    Bio::Command.query_command(cmd)
   end
   
-  #query the file 
-  def query(file_name)
-    cmd = make_command
-    exec_local(cmd)
-  end  
-  
-  # TODO create a parser class for the ouput
-  # parse_results
-  
- private
- #executes bepipred when called localy
- #The input is a file name or a path to the file containing protein sequences in fasta format
- #This method does not work
- # There could be a bug in the way the cmd aregument is created.
- def exec_local(cmd)
-   Bio::Command.query_command(cmd)
- end
-    
-end  
 end
+end
+
+
+b = Bio::Bepipred.new
+puts b.make_command()
